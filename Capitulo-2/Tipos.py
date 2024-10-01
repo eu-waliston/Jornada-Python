859f5b9dbfc2665e1b7b360879d772a775bb6cfb
Waliston <wesantosdev@gmail.com>
2024-10-01 16:10:31 -0300

    fim do segundo  capitulo

---
+++ Capitulo-2/Tipos.py
@@ -0,0 +1,19 @@
+#tipos de dados no python
+
+#String >> segfuqncias de caracteres ex:
+nome = "Waliston"
+print(nome.capitalize())
+
+#Numerico, podedm ser rtipos inteiros e de ponto flutiante e complexos
+numero_complexo = complex(1,4)
+a = 19
+b = 9.1
+print(a)
+print(a + b)
+print(numero_complexo)
+#Logico ou boolean e podem ser dois vcalores true e flase
+
+print('true')
+print('false')
+
+#dados avançados ou complexos [ lista, dicionarios, setts, tuplas]
