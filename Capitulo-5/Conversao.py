c29d82c691f5a8e277feff3e6ac269a4177f9531
Waliston <wesantosdev@gmail.com>
2024-10-25 14:48:18 -0300

    Capitulo 5

---
+++ Capitulo-5/Conversao.py
@@ -0,0 +1,10 @@
+idade = 52
+pontuacao_str = '10'
+#casting de valores em python
+#str()
+#int()
+#float()
+texto = "Parabens João pelos seus " + str(idade) + " anos de idade"
+print(texto)
+print(f'Tip: {type(int(pontuacao_str))}')
+
---
+++ Capitulo-5/Tratamento.py
@@ -0,0 +1,22 @@
+n_1 = 50
+n_2 = -4
+
+#abs -> retorna a distancia do numero ate o ZERO (mesmo que seja negativo)
+print('abs')
+print(abs(n_1))
+print(abs(n_2))
+print(abs(n_2))
+
+# round -> serve para arredondar os numeros
+print('\n')
+print('round')
+print(round(n_1))
+print(round(5.5))
+print(round(5.234234, 1))
+
+# pow -> função de potencia no python
+print('\n')
+print('pow')
+print(pow(2, 3))
+
+
