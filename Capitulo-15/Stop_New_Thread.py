a2ba2501d0e767ce5e10f03f036eab5f096aa15a
Waliston <wesantosdev@gmail.com>
2024-11-02 15:04:03 -0300

    Capitulo 15 FIM

---
+++ Capitulo-15/Stop_New_Thread.py
@@ -0,0 +1,33 @@
+# SuperFastPython.com
+# example of stopping a new thread
+from time import sleep
+from threading import Thread
+from threading import Event
+
+#custom task function
+def task(event):
+    #execute a task in a loop
+    for i in range(5):
+        #block for a moment
+        sleep(1)
+        #check for stop
+        if event.is_set():
+            break
+        #report a message
+        print("Worker thread running...")
+    print("worker closing down")
+
+#create the event
+event = Event()
+
+#create and configuring a new thread
+thread = Thread(target=task, args=(event,))
+#start tne new thread
+thread.start()
+#block for a while
+sleep(3)
+#stop the worker thread
+print("Main stopping thread")
+event.set()
+#wait for the new thread to finish
+thread.join()
